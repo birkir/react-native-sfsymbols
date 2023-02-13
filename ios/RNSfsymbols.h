a4e850ed7c0f5c10957ed23dd260c98c7ee10c94
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2023-02-13 09:45:09 +0000

    feat: new header

--- ios/RNSfsymbols.h
+++ ios/RNSfsymbols.h
@@ -1,5 +1,16 @@
-#import <React/RCTBridgeModule.h>
+#import <React/RCTComponent.h>
+#import <React/RCTBridge.h>
+#import <UIKit/UIKit.h>
 
-@interface RNSfsymbols : NSObject <RCTBridgeModule>
+@interface RNSfsymbols : UIImageView<RCTComponent>
+
+- (instancetype)initWithBridge:(RCTBridge *)bridge;
+
+@property (nonatomic, assign) BOOL multiColor;
+@property (nonatomic, assign) UIImageSymbolWeight weight;
+@property (nonatomic, assign) UIImageSymbolScale scale;
+@property (nonatomic, assign) CGFloat size;
+@property (nonatomic, assign) UIViewContentMode resizeMode;
+@property (nonatomic, copy) NSString *systemName;
 
 @end
